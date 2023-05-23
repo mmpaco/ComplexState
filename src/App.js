3ead881d71a26f0b7fca5f65572e829f1001766b
mmpaco <122134870+mmpaco@users.noreply.github.com>
2023-05-23 10:09:12 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  return (
+    <div className="App">
+      <h1>
+        Hello {fName} {lName}{" "}
+      </h1>
+
+      <input
+        type="text"
+        placeHolder="First Name"
+        onChange={(e) => setFName(e.target.value)}
+      ></input>
+
+      <input
+        type="text"
+        placeHolder="Last Name"
+        onChange={(e) => setLName(e.target.value)}
+      ></input>
+    </div>
+  );
+}
